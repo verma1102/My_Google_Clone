fd061ea6acee155192ccadb47893cab713355c20
vPiyush1102 <piyush.verma@squbix.com>
2022-11-28 15:53:55 +0530

    Updated Dependencies Version & fixed some Bugs.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import './App.css';
-import Home from './Pages/Home';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import SearchPage from './Pages/SearchPage';
+import React from "react";
+import Home from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
+import SearchPage from "./Pages/SearchPage";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,48 +1,58 @@
-import React, { useState } from 'react';
-import './Search.css';
-import SearchIcon from '@mui/icons-material/Search';
-import MicIcon from '@mui/icons-material/Mic';
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
-import { useNavigate } from 'react-router-dom';
-import { useStateValue } from './StateProvider';
-import { actionTypes } from './reducer';
+import { useNavigate } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
 
 function Search({ hideButtons = false }) {
-    const [{ }, dispatch] = useStateValue();
-    const [input, setInput] = useState("");
-    const Navigate = useNavigate();
+  const [dispatch] = useStateValue();
+  const [input, setInput] = useState("");
+  const Navigate = useNavigate();
 
-    const search = e => {
-        e.preventDefault();
+  const search = (e) => {
+    e.preventDefault();
 
-        console.log('You Hit the Search Button');
+    console.log("You Hit the Search Button");
 
-        dispatch({
-            type: actionTypes.SET_SEARCH_TERM,
-            term: input
-        })
-        Navigate('./search');
-    }
-    return (
-        <form className='search'>
-            <div className='search__input'>
-                <SearchIcon className="search__inputIcon" />
-                <input onChange={e => setInput(e.target.value)}
-                    value={input} />
-                <MicIcon />
-            </div>
-            {!hideButtons ? (
-                <div className='search__buttons'>
-                    <Button type='submit' onClick={search} variant="outlined">Google Search</Button>
-                    <Button variant="outlined">I'm Feeling Lucky</Button>
-                </div>
-            ) : (
-                <div className='search__buttons'>
-                    <Button className='search_buttonsHidden' type='submit' onClick={search} variant="outlined">Google Search</Button>
-                    <Button className='search_buttonsHidden' variant="outlined">I'm Feeling Lucky</Button>
-                </div>
-            )}
-        </form>
-    )
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
+    Navigate("./search");
+  };
+  return (
+    <form className="search">
+      <div className="search__input">
+        <SearchIcon className="search__inputIcon" />
+        <input onChange={(e) => setInput(e.target.value)} value={input} />
+        <MicIcon />
+      </div>
+      {!hideButtons ? (
+        <div className="search__buttons">
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm Feeling Lucky</Button>
+        </div>
+      ) : (
+        <div className="search__buttons">
+          <Button
+            className="search_buttonsHidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button className="search_buttonsHidden" variant="outlined">
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      )}
+    </form>
+  );
 }
 export default Search;
